1a3ee6f6e12ca28c03099fecd994dc4249deb003
stbuccia <stbuccia@yahoo.it>
2017-05-08 16:34:43 +0200

    ComboBox

--- src/controller/WelcomeWindowController.java
+++ src/controller/WelcomeWindowController.java
@@ -1,5 +1,33 @@
 package controller;
 
-public class WelcomeWindowController {
+import java.awt.event.ActionEvent;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
+public class WelcomeWindowController {
+	/*
+	 @FXML
+	 private ComboBox<?> categoryComboBox;
+	 public void setCategoryComboBox(ComboBox<String> categoryComboBox) {
+	     this.categoryComboBox = categoryComboBox;
+	 }
+
+	 public ComboBox<String> getCategoryComboBox() {
+	     return (ComboBox<String>) categoryComboBox;
+	 }
+
+	 @FXML
+	 private void comboBoxSelection(ActionEvent event) {
+		 /*categoryComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+	         public void changed(ObservableValue<? extends String> ov, 
+	             String old_val, String new_val) {
+	                String categoryStr =  getCategoryComboBox().getValue().toString(); //Don't know what to put here
+	     }
+	      });
+		 System.out.println("Ciao");
+	 }*/
+	
 }
